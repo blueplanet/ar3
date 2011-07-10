d961fddf8a7e6c9e2e83fe5669773e944b173b08
erguolinge <erguolinge@gmail.com>
2011-07-11 07:38:01 +0900

    D1:カートをサイドバーに移動

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,12 +1,14 @@
 class StoreController < ApplicationController
   def index
     @products = Product.find_products_for_sale
+    @cart = find_cart
   end
 
   def add_to_cart
     product = Product.find(params[:id])
     @cart = find_cart
     @cart.add_product(product)
+    redirect_to_index
   rescue ActiveRecord::RecordNotFound
     logger.error("無効な商品ID#{params[:id]}にアクセスしようとしました")
     redirect_to_index("無効な商品です")
@@ -17,8 +19,8 @@ class StoreController < ApplicationController
     redirect_to_index("カートは現在空です")
   end
   
-  def redirect_to_index(msg)
-    flash[:notice] = msg
+  def redirect_to_index(msg = nil)
+    flash[:notice] = msg if msg
     redirect_to :action => 'index'
   end
 
