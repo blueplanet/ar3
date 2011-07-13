3d07713b71aca88ea28c9c297362966166b59273
erguolinge <erguolinge@gmail.com>
2011-07-14 08:11:59 +0900

    store多言語対応

---
+++ config/initializers/i18n.rb
@@ -0,0 +1,10 @@
+I18n.default_locale = 'en'
+
+LOCALES_DIRECTORY = "#{RAILS_ROOT}/config/locales/"
+
+LANGUAGES = {
+  'English' => 'en',
+  "Espa\xc3\xblol" => 'es',
+  'Japanease' => 'ja',
+  'Chinease' => 'cn'
+}
