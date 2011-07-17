37eaa171369cf781dea35021498d651a1b45557e
erguolinge <erguolinge@gmail.com>
2011-07-17 23:08:07 +0900

    setup

--- test/unit/cart_test.rb
+++ test/unit/cart_test.rb
@@ -2,28 +2,27 @@ require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
   fixtures :products
-  test "add unique products" do
-    cart = Cart.new
-    rails_book = products(:rails_book)
-    ruby_book = products(:ruby_book)
 
-    cart.add_product rails_book
-    cart.add_product ruby_book
-
-    assert_equal 2, cart.items.size
-    assert_equal rails_book.price + ruby_book.price, cart.total_price
+  def setup
+    @cart = Cart.new
+    @rails = products(:rails_book)
+    @ruby = products(:ruby_book)
   end
 
-  test "add duplicate product" do
-    cart = Cart.new
-    rails_book = products(:rails_book)
-    ruby_book = products(:ruby_book)
+  test "add unique products" do
+    @cart.add_product @rails
+    @cart.add_product @ruby
 
-    cart.add_product rails_book
-    cart.add_product rails_book
+    assert_equal 2, @cart.items.size
+    assert_equal @rails.price + @ruby.price, @cart.total_price
+  end
 
-    assert_equal 2 * rails_book.price, cart.total_price
-    assert_equal 1, cart.items.size
-    assert_equal 2, cart.items[0].quantity
+  test "add duplicate roduct" do
+    @cart.add_product @rails
+    @cart.add_product @rails
+
+    assert_equal 2 * @rails.price, @cart.total_price
+    assert_equal 1, @cart.items.size
+    assert_equal 2, @cart.items[0].quantity
   end
 end
