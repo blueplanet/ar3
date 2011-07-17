1d79953a1ab867d9210d924529ea84c23a004ecc
erguolinge <erguolinge@gmail.com>
2011-07-18 00:00:21 +0900

    controller:admin#index test

--- test/functional/admin_controller_test.rb
+++ test/functional/admin_controller_test.rb
@@ -1,9 +1,17 @@
 require 'test_helper'
 
 class AdminControllerTest < ActionController::TestCase
+  fixtures :users
+
   test "index without user" do
     get :index
     assert_redirected_to :action => "login"
     assert_equal "Please Login", flash[:notice]
+  end
+
+  test "index with user" do
+    get :index, {}, { :user_id => users(:dave).id }
+    assert_response :success
+    assert_template "index"
   end
 end
